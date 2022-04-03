4e7f28cee75c5bef995f154b71b508401bed506a
Kirill Nizhnik <kiril.niznik@gmail.com>
2022-04-03 15:30:18 +0300

    NEW CLASS AND FXML FILE, new function APP

---
+++ src/main/java/com/example/test/ControllerMenuComparisonOfTwoAccounts.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerMenuComparisonOfTwoAccounts {
+    private String ACCOUNT_NAME_1;
+    private String ACCOUNT_NAME_2;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button compareAccounts;
+
+    @FXML
+    private Button backToMenuButton;
+
+    @FXML
+    private TextField nickNameFirstInstagramAccountTextField;
+
+    @FXML
+    private TextField nickNameSecondInstagramAccountTextField1;
+
+    @FXML
+    void initialize() {
+
+    }
+
+
+}
